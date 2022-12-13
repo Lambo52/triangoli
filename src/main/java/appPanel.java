43c28848ba03ff210e9afc2ca5f7048e2ad42496
Lambo52 <francesco.la52@gmail.com>
2022-12-13 15:05:46 +0100

    Initial commit

---
+++ src/main/java/appPanel.java
@@ -0,0 +1,119 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class appPanel extends JPanel implements Runnable{
+
+    double drawInterval;
+    double delta;
+    long lastTime;
+    long currentTime;
+    MouseEvent mouseClicked;
+    int FPS = 60;
+    mouseInputs mouseListener = new mouseInputs(this);
+    Thread gameThread; //gameThread è il thread principale per il loop
+    boolean running = false; //serve per non far ripartire la thread
+
+    Graphics g;
+
+    Point[] point = new Point[3];
+    Point point1 = new Point(100,500);
+    Point point2 = new Point(700,500);
+    Point point3 = new Point(400,100);
+    Point newPoint = new Point();
+    Point randomPoint = new Point();
+
+    boolean startLoop = false;
+
+    public appPanel() {
+        this.setPreferredSize(new Dimension(800,600)); //dimensioni pannello
+
+        this.setBackground(Color.white);
+        this.setDoubleBuffered(true); //incrementa le performances (in realtà non serve a niente)
+
+        this.addMouseListener(mouseListener); //implementa i clic del mouse
+        this.addMouseMotionListener(mouseListener); //implementa i movimenti del mouse
+
+        this.requestFocus();// dovrebbe servire per il mouse ma funziona anche senza dato che c'è setFocusable
+        this.setFocusable(true);
+        point[0] = point1;
+        point[1] = point2;
+        point[2] = point3;
+
+    }
+
+
+   public void startThread(){
+
+        if (running) {
+            return; //ritorna se la thread è gia partita e non riparte 2 volte, per sicurezza
+        }
+        running = true;
+        gameThread = new Thread(this); //qui si inizializza in Thread
+        gameThread.start(); //questo chiama il metodo run direttamente
+    }
+
+
+    @Override
+    public void run() { //metodo generato automaticamente da runnable
+/*
+        drawInterval = 1000000000/FPS;
+        delta = 0;
+        lastTime = System.nanoTime();
+        //long currentTime;
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime(); // misura in miliardo di nanosecondi
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                paint(g);
+                delta--;
+            }
+        }
+        */
+        while(true) {
+            paint(g);
+        }
+    }
+
+
+
+    public void paint(Graphics g) {
+        g = getGraphics();
+        g.setColor(Color.black);
+
+        g.fillRect(point1.x,point1.y,1,1);
+        g.fillRect(point2.x,point2.y,1,1);
+        g.fillRect(point3.x, point3.y, 1,1);
+
+        if(startLoop) {
+            Random random = new Random();
+            int i = random.nextInt(3);
+            randomPoint = point[i];
+
+            newPoint.x = (int) (newPoint.x + randomPoint.x) / 2;
+            newPoint.y = (int) (newPoint.y + randomPoint.y) / 2;
+
+            g.fillRect(newPoint.x, newPoint.y, 1,1);
+        }
+
+
+        g.dispose();
+
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        mouseClicked = e;
+        newPoint.x = e.getX();
+        newPoint.y = e.getY();
+        startLoop = true;
+    }
+
+}
---
+++ src/main/java/mouseInputs.java
@@ -0,0 +1,40 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class mouseInputs implements MouseListener, MouseMotionListener {
+    appPanel aP;
+    public mouseInputs(appPanel appPanel) {
+        this.aP = appPanel;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        aP.mouseClicked(e);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+}
